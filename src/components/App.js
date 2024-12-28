1d742ee69723225187df7773e1084a4db1819c2b
saurabhanand96310 <122476256+saurabhanand96310@users.noreply.github.com>
2024-12-28 10:11:04 +0000

    added

--- src/components/App.js
+++ src/components/App.js
@@ -1,42 +1,52 @@
-import React, { Component, useState } from "react";
-import '../styles/App.css';
+import React, { Component } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            renderBall: false,
-            posi : 0,
-            ballPosition: { left: "0px" }
-        };
-        this.renderChoice = this.renderBallOrButton.bind(this)
-        this.buttonClickHandler = this.buttonClickHandler.bind(this)
+  constructor(props) {
+    super(props);
+    this.state = {
+      renderBall: false,
+      ballPosition: { left: "0px" },
     };
+    this.renderBallOrButton = this.renderBallOrButton.bind(this);
+    this.buttonClickHandler = this.buttonClickHandler.bind(this);
+    this.handleArrowRight = this.handleArrowRight.bind(this); // Bind the method
+  }
 
-    buttonClickHandler() {
-   
-   }
-    renderBallOrButton() {
-		if (this.state.renderBall) {
-		    return <div className="ball" style={this.state.ballPosition}></div>
-		} else {
-		    return <button onClick={this.buttonClickHandler} >Start</button>
-		}
-    }
+  buttonClickHandler() {
+    this.setState({ renderBall: true });
+  }
 
-    // bind ArrowRight keydown event
-    componentDidMount() {
-      
+  renderBallOrButton() {
+    if (this.state.renderBall) {
+      return <div className="ball" style={this.state.ballPosition}></div>;
+    } else {
+      return <button onClick={this.buttonClickHandler}>Start</button>;
     }
+  }
 
-    render() {
-        return (
-            <div className="playground">
-                {this.renderBallOrButton()}
-            </div>
-        )
+  handleArrowRight(e) {
+    if (e.key === "ArrowRight") {
+      // Extract the current left position and calculate the new position
+      const currentLeft = parseInt(this.state.ballPosition.left, 10);
+      const newLeft = currentLeft + 5;
+      this.setState({
+        ballPosition: { left: `${newLeft}px` },
+      });
     }
+  }
+
+  componentDidMount() {
+    window.addEventListener("keydown", this.handleArrowRight); // Add keydown listener
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("keydown", this.handleArrowRight); // Cleanup the listener
+  }
+
+  render() {
+    return <div className="playground">{this.renderBallOrButton()}</div>;
+  }
 }
-
 
 export default App;
